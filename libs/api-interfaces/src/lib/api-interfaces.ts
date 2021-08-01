3aebc785e4a9b1bf2b686f382dfad37dbf1e1cad
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2021-08-01 11:21:25 +0300

    feat(LIB): added interfaces

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,12 @@
-export interface Message {
-  message: string;
+class SocketMessage<T> {
+  public payload: T;
+  constructor(public action: string) {}
 }
+
+export type PayloadType<T> = T extends { payload: unknown }
+  ? T['payload']
+  : unknown;
+
+// [from]_$[emit type]
+export const client_$message = new SocketMessage<string>('client_$message');
+export const server_$message = new SocketMessage<string>('MESSAGE_FROM_CLIENT');
